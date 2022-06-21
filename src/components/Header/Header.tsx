f9a67e9872bc752aebfe0f629f5c902ecebfd3df
hyemin065 <p62517@gmail.com>
2022-06-21 23:34:04 +0900

    feat: main 카테고리, header 마크업,css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,19 @@ import styles from './header.module.scss';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>WeSpace</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='/signin'>로그인</NavLink>
-          </li>
-          <li>
-            <NavLink to='/signup'>회원가입</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.container}>
+        <h1>WeSpace</h1>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <NavLink to='/signin'>로그인</NavLink>
+            </li>
+            <li>
+              <NavLink to='/signup'>회원가입</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,50 +4,60 @@ import styles from './home.module.scss';
 const Home = () => {
   return (
     <div className={styles.homeWrap}>
-      <h2>
-        공간을 <br /> 공유하다
-      </h2>
       <div className={styles.container}>
-        <ul className={styles.box}>
+        <h2>
+          공간을 <br /> 공유하다
+        </h2>
+        <ul className={styles.categoryWrap}>
           <li>
-            <dl>
-              <dt>
-                <BookIcon />
-              </dt>
-              <dd>스터디</dd>
-            </dl>
+            <button type='button'>
+              <dl>
+                <dt>
+                  <BookIcon />
+                </dt>
+                <dd>스터디</dd>
+              </dl>
+            </button>
           </li>
           <li>
-            <dl>
-              <dt>
-                <GroupsIcon />
-              </dt>
-              <dd>모임</dd>
-            </dl>
+            <button type='button'>
+              <dl>
+                <dt>
+                  <GroupsIcon />
+                </dt>
+                <dd>모임</dd>
+              </dl>
+            </button>
           </li>
           <li>
-            <dl>
-              <dt>
-                <MeetingIcon />
-              </dt>
-              <dd>회의</dd>
-            </dl>
+            <button type='button'>
+              <dl>
+                <dt>
+                  <MeetingIcon />
+                </dt>
+                <dd>회의</dd>
+              </dl>
+            </button>
           </li>
           <li>
-            <dl>
-              <dt>
-                <PartyIcon />
-              </dt>
-              <dd>파티</dd>
-            </dl>
+            <button type='button'>
+              <dl>
+                <dt>
+                  <PartyIcon />
+                </dt>
+                <dd>파티</dd>
+              </dl>
+            </button>
           </li>
           <li>
-            <dl>
-              <dt>
-                <EtcIcon />
-              </dt>
-              <dd>기타</dd>
-            </dl>
+            <button type='button'>
+              <dl>
+                <dt>
+                  <EtcIcon />
+                </dt>
+                <dd>기타</dd>
+              </dl>
+            </button>
           </li>
         </ul>
       </div>

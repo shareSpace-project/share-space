4a8f7e02a36edbb3a6a60e454c51f494135f6517
unknown <wnghks0531@naver.com>
2022-07-01 17:58:17 +0900

    style:login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,9 +4,24 @@ import img from '../../assets/image2.jpg';
 
 const Login = () => {
   return (
-    <div className={styles.container}>
-      <h2>11</h2>
-      <img src={img} alt='error' />
+    <div className={styles.loginContainer}>
+      <div className={styles.loginForm}>
+        <h2>Welcome Back</h2>
+        <form>
+          <label htmlFor='email'>Email</label>
+          <input type='text' id='email' />
+
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' />
+
+          <button type='submit' className={styles.loginButton}>
+            Login
+          </button>
+        </form>
+      </div>
+      <div>
+        <img src={img} alt='error' />
+      </div>
     </div>
   );
 };
